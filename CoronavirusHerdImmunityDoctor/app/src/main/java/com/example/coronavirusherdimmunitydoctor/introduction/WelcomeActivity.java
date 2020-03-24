d75e4e0def900bb146ecfb3a4a10f623bc4c35f1
Elena Martina <elenamartina98@gmail.com>
2020-03-24 16:27:19 +0000

    Welcome/how it works

--- CoronavirusHerdImmunityDoctor/app/src/main/java/com/example/coronavirusherdimmunitydoctor/introduction/WelcomeActivity.java
+++ CoronavirusHerdImmunityDoctor/app/src/main/java/com/example/coronavirusherdimmunitydoctor/introduction/WelcomeActivity.java
@@ -2,10 +2,12 @@ package com.example.coronavirusherdimmunitydoctor.introduction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.coronavirusherdimmunitydoctor.R;
@@ -26,6 +28,9 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.intro0_welcome);
 
 
+        this.writeTitle();
+
+
         Button start_button = (Button) findViewById(R.id.button_next);
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +49,18 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+
+    private void writeTitle() {
+        String first = getResources().getString(R.string.welcome_first);
+        String blue = getResources().getString(R.string.welcome_next);
+        String last = getResources().getString(R.string.welcome_last);
+        TextView t = (TextView) findViewById(R.id.welcome_to);
+
+        t.setText(Html.fromHtml(first +
+                "<font color='#16ACEA'>"+blue+"</font>"
+                + last));
     }
 }
